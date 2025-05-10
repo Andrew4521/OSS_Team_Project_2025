36681cff2f398f19b3ba33f8effb68009f6ceea6
Leegyuone <113154272+Leegyuone@users.noreply.github.com>
2025-05-10 17:28:27 +0900

    전공 데이터 받아옴
    전공, 입학년도, 복수전공, 복수전공 시작년도를 가져오는 프로그램입니다.
    각각의 정보를 딕셔너리 형태로 받아왔습니다.

---
+++ major_data.py
@@ -0,0 +1,73 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import getpass
+
+#최신 버전 크롬에 맞는 크롬드라이버 설치
+service = Service(ChromeDriverManager(driver_version="136.0.7103.49").install())
+driver = webdriver.Chrome(service=service)
+
+#로그인 페이지 접속
+driver.get("https://eis.cbnu.ac.kr/cbnuLogin")
+
+#로그인
+username_field = driver.find_element(By.NAME, "uid")
+password_field = driver.find_element(By.NAME, "pswd")
+
+#아이디, 비밀번호 입력
+id = getpass.getpass()
+pw = getpass.getpass()
+
+username_field.send_keys(f"{id}")  #실제 아이디 입력
+password_field.send_keys(f"{pw}")  #실제 비밀번호 입력
+password_field.send_keys(Keys.RETURN)  #엔터 키 입력
+time.sleep(20) #20초 대기
+
+#로그인 후 학점 이수 내역 페이지로 이동
+driver.get("https://eisn.cbnu.ac.kr/nxui/index.html?OBSC_YN=0&LNG=ko#14295")
+time.sleep(10)
+
+#입학년도, 전공, 복수전공 가져와 저장
+majors = {
+    "전공":"",
+    "입학년도":"",
+    "복수전공":"",
+    "복수전공 시작년도":""
+    }
+
+try:
+
+    for i in ["05","03","16"]: #데이터 가져와 위 딕셔너리에 저장
+        element = WebDriverWait(driver, 30).until(
+        EC.visibility_of_element_located((By.XPATH, f'//*[@id="mainframe.WrapFrame.form.div_section.form.div_content.form.div_work.form.w_14295.form.div_work.form.tab_main.tpg_1.form.div_detail.form.edt_txt{i}:input"]'))
+)
+        if i == "05": #전공 저장
+            major = element.get_attribute("value")
+            print(major)
+            majors["전공"] = major
+        
+        elif i == "03": #입학년도 저장
+            welcome = element.get_attribute("value")
+            print(welcome)
+            majors["입학년도"] = welcome
+
+        else: #복수전공 시작년도와 복수전공을 나눠서 저장
+            year_second_major = element.get_attribute("value")
+            print(year_second_major)
+            year_major = year_second_major.split()
+            majors["복수전공"] = year_major[1]
+            majors["복수전공 시작년도"] = year_major[0]
+
+    
+    for key, value in majors.items():
+        print(f"{key}, {value}")
+
+
+except Exception as e:
+    print(f"예외 {e}")
+
