b6d675aeed88e8658690fe2377a548be33be825e
qtkqhg <bakhosang@gmail.com>
2025-05-11 16:33:05 +0900

    Student 클래스 변수 변경

--- 새로운 마음/새로운 마음/Student.h
+++ 새로운 마음/새로운 마음/Student.h
@@ -1,30 +1,19 @@
+// Student.h
 #pragma once
+#include <string>
+#include "Credit.h"
 
-#include <string>  
 
 class Student {
-public:    
-    Student(const std::string& major, int minMaj, int maxMaj, int completedMaj, int minGenEd, int maxGenEd, int completedGenEd);
-   
-
-    const std::string& getMajor();                   // 학과 
-    int getMinMajorCredits();                        // 최소 전공 학점 
-    int getMaxMajorCredits();                        // 최대 전공 학점 
-    int getCompletedMajorCredits();                  // 수강한 전공 학점 
-    int getMinGenEdCredits();                        // 최소 교양 학점 
-    int getMaxGenEdCredits();                        // 최대 교양 학점 
-    int getCompletedGenEdCredits();                  // 수강한 교양 학점 
+public:
+    using Json = nlohmann::json;
+    // JSON 객체로부터 completed/required credit을 초기화
+    explicit Student(const Json& j);
 
 
 
-private:
-    std::string major;                     // 학과
-    std::string secondMajor;               // 복수 전공 학과
-    int minMajorCredits;                   // 최소 전공 학점
-    int maxMajorCredits;                   // 최대 전공 학점
-    int completedMajorCredits;             // 수강한 전공 학점
 
-    int minGenEdCredits;                   // 최소 교양 학점
-    int maxGenEdCredits;                   // 최대 교양 학점
-    int completedGenEdCredits;             // 수강한 교양 학점
+public:
+    Cp_Credit cp_credit;  // 이수 학점
+    Rq_Credit rq_credit;  // 요구 학점
 };
