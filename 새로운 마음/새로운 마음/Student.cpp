56bcd0547361c3d294f8a9ac0b627b21074f0278
qtkqhg <bakhosang@gmail.com>
2025-05-11 16:35:20 +0900

    Student 생성자 구현부 수정

--- 새로운 마음/새로운 마음/Student.cpp
+++ 새로운 마음/새로운 마음/Student.cpp
@@ -1,43 +1,9 @@
+// Student.cpp
 #include "Student.h"
 
-Student::Student(const std::string& major,
-    int minMaj, int maxMaj, int completedMaj,
-    int minGenEd, int maxGenEd, int completedGenEd)
-    : major(major)
-    , minMajorCredits(minMaj)
-    , maxMajorCredits(maxMaj)
-    , completedMajorCredits(completedMaj)
-    , minGenEdCredits(minGenEd)
-    , maxGenEdCredits(maxGenEd)
-    , completedGenEdCredits(completedGenEd)
+
+Student::Student(const Json& j)
+    : rq_credit(j),
+    cp_credit(j)
 {
-}
-
-const std::string& Student::getMajor() {
-    return major;
-}
-
-
-int Student::getMinMajorCredits() {
-    return minMajorCredits;
-}
-
-int Student::getMaxMajorCredits() {
-    return maxMajorCredits;
-}
-
-int Student::getCompletedMajorCredits() {
-    return completedMajorCredits;
-}
-
-int Student::getMinGenEdCredits() {
-    return minGenEdCredits;
-}
-
-int Student::getMaxGenEdCredits() {
-    return maxGenEdCredits;
-}
-
-int Student::getCompletedGenEdCredits() {
-    return completedGenEdCredits;
 }
