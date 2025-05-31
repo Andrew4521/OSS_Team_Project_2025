731c2cd50a5aab1f1145d6aa43179dbaf7351a37
Leegyuone <113154272+Leegyuone@users.noreply.github.com>
2025-05-31 22:02:41 +0900

    Update all_data_get.py
    필요 없는 문장 삭제
    함수를 만들어 사용 > 예외 구문 활용

--- all_data_get.py
+++ all_data_get.py
@@ -31,16 +31,12 @@ time.sleep(20) #20초 대기
 
 login_xpath = '//*[@id="mainframe.login.form.btn_yes"]'
 
-def New_login_check(xpath):
-    try:
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, xpath)))
-        return True
-    except:
-        return False
-
-if New_login_check:
+try:
     New_login = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, login_xpath)))
     New_login.click()
+
+except:
+    pass
 
 time.sleep(5)
 
@@ -120,7 +116,6 @@ for i in range(1, 44):
         sub_credit_value = element.get_attribute("aria-label")
         sub_credit.append(sub_credit_value)
         print(sub_credit[-1])
-        driver.switch_to.default_content()
 
     except Exception as e:
         print(f"요소를 찾을 수 없음: {e}")
@@ -151,41 +146,34 @@ for i in range(7):
 
 row = 0
 col = 0
+multi_list = []
 xpath = f'//*[@id="mainframe.WrapFrame.form.div_section.form.div_content.form.div_work.form.w_14295.form.div_work.form.tab_main.tpg_3.form.grd_tpg3List.body.gridrow_{row}.cell_{row}_{col}"]'
 
-#XPATH 존재 우뮤 확인 함수
-def xpath_t_f(driver, xapth):
-    try:
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xapth)))
-        return True
 
-    except:
-        return False
-    
-is_xpath_row = xpath_t_f(driver, xpath)
-multi_list = []
+try:
+    while True: #만약 XPATH가 존재한다면 해당 데이터를 가져오고 다음 행렬 위치 값을 XPATH에 넣어줌.
+        inner_list = [] #각 행을 저장할 리스트
 
-while is_xpath_row: #만약 XPATH가 존재한다면 해당 데이터를 가져오고 다음 행렬 위치 값을 XPATH에 넣어줌.
-    inner_list = [] #각 행을 저장할 리스트
-
-    for i in range(10):
-        element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, xpath))
-)
+        for i in range(10):
+            element = WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, xpath))
+    )
         
-        inner_list.append(element.get_attribute("aria-label"))
-        col += 1
+            inner_list.append(element.get_attribute("aria-label"))
+            col += 1
 
-        if col == 10: #열의 범위를 넘어가면 초기화
-            col = 0
+            if col == 10: #열의 범위를 넘어가면 초기화
+                col = 0
         
-        xpath = f'//*[@id="mainframe.WrapFrame.form.div_section.form.div_content.form.div_work.form.w_14295.form.div_work.form.tab_main.tpg_3.form.grd_tpg3List.body.gridrow_{row}.cell_{row}_{col}"]'
+            xpath = f'//*[@id="mainframe.WrapFrame.form.div_section.form.div_content.form.div_work.form.w_14295.form.div_work.form.tab_main.tpg_3.form.grd_tpg3List.body.gridrow_{row}.cell_{row}_{col}"]'
 
-    multi_list.append(inner_list)
+        multi_list.append(inner_list)
 
-    row += 1
-    xpath = f'//*[@id="mainframe.WrapFrame.form.div_section.form.div_content.form.div_work.form.w_14295.form.div_work.form.tab_main.tpg_3.form.grd_tpg3List.body.gridrow_{row}.cell_{row}_{col}"]' #다음 요소의 XPATH 저장
-    is_xpath_row = xpath_t_f(driver, xpath) #존재유무 확인
+        row += 1
+        xpath = f'//*[@id="mainframe.WrapFrame.form.div_section.form.div_content.form.div_work.form.w_14295.form.div_work.form.tab_main.tpg_3.form.grd_tpg3List.body.gridrow_{row}.cell_{row}_{col}"]' #다음 요소의 XPATH 저장
+
+except:
+    print("오류 발생")
     
 
 for i in multi_list:
